c960a2073845205599532c3fd97a9d97593ad61b
Atsushi Nakagawa <dkahw210@kyoto.zaq.ne.jp>
2015-11-20 23:06:17 +0900

    added files

---
+++ src/lisage/lisage.cpp
@@ -0,0 +1,258 @@
+// lisage.cpp : アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include "lisage.h"
+#include <math.h>
+
+#define MAX_LOADSTRING 100
+#define WM_TRAYICON		WM_APP
+
+// グローバル変数:
+HINSTANCE hInst;								// 現在のインターフェイス
+TCHAR szTitle[MAX_LOADSTRING];					// タイトル バーのテキスト
+TCHAR szWindowClass[MAX_LOADSTRING];			// メイン ウィンドウ クラス名
+
+// このコード モジュールに含まれる関数の宣言を転送します:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+BOOL CALLBACK MoveWindow(HWND hWnd, LPARAM lParam);
+
+int count = 0; // カウンタ
+double sinn[80], coss[40]; // 力技
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPTSTR    lpCmdLine,
+                     int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+ 	// TODO: ここにコードを挿入してください。
+	MSG msg;
+	HACCEL hAccelTable;
+
+	if (MessageBox(NULL, L"本ソフトウェアを実行すると、画面上のすべてのウィンドウが動き出し、\n"
+						 L"予期しない動作を引き起こしたり、動作に支障をきたす恐れがあります。\n"
+						 L"右下のタスクトレイを右クリックことで終了できますが、\n念のため事前にタスクマネージャーを起動しておくことを強くおすすめします。\n\n"
+						 L"実行しますか？", L"!! WARNING !!", MB_YESNO | MB_ICONWARNING) == IDNO)
+		return 0;
+
+	// グローバル文字列を初期化しています。
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_LISAGE, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// アプリケーションの初期化を実行します:
+	if (!InitInstance (hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_LISAGE));
+
+	// テーブル設定
+	int i;
+		for (i = 0; i < 80; i++)
+		{
+			sinn[i] = sin((double)i * 3.1415926535897932 / 40) * 16;
+			if (i < 40)
+				coss[i] = cos((double)i * 3.1415926535897932 / 20) * 4;
+		}
+
+	// メイン メッセージ ループ:
+	while (1)
+	{
+		PeekMessage(&msg, NULL, 0, 0, PM_REMOVE);
+		if (msg.message == WM_QUIT) break;
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		EnumWindows(MoveWindow, NULL);
+			count++;
+		Sleep(16);	//　ネタも無いので、今日はこのへんで終わりにしようかと思います。
+					//　くだらない内容で申し訳ないですが、ご視聴どうもありがとうございました。
+					//　テロップの書き換え画面が戻ってこない・・・
+	}
+
+	return (int) msg.wParam;
+}
+
+
+
+//
+//  関数: MyRegisterClass()
+//
+//  目的: ウィンドウ クラスを登録します。
+//
+//  コメント:
+//
+//    この関数および使い方は、'RegisterClassEx' 関数が追加された
+//    Windows 95 より前の Win32 システムと互換させる場合にのみ必要です。
+//    アプリケーションが、関連付けられた
+//    正しい形式の小さいアイコンを取得できるようにするには、
+//    この関数を呼び出してください。
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_LISAGE));
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_LISAGE);
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   関数: InitInstance(HINSTANCE, int)
+//
+//   目的: インスタンス ハンドルを保存して、メイン ウィンドウを作成します。
+//
+//   コメント:
+//
+//        この関数で、グローバル変数でインスタンス ハンドルを保存し、
+//        メイン プログラム ウィンドウを作成および表示します。
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   HWND hWnd;
+
+   hInst = hInstance; // グローバル変数にインスタンス処理を格納します。
+
+   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   return TRUE;
+}
+
+BOOL CALLBACK MoveWindow(HWND hWnd, LPARAM lParam)
+{
+	if (IsWindowVisible(hWnd))
+	{
+		RECT point;
+		int scrambled_count = count + (int)hWnd;
+
+		GetWindowRect(hWnd, &point);
+		SetWindowPos(hWnd, HWND_TOP, point.left + sinn[scrambled_count % 80] * (scrambled_count / 80 % 2 ? -1 : 1), point.top + coss[scrambled_count % 40],
+			0, 0, SWP_NOSIZE);
+	}
+	return TRUE;
+}
+
+//
+//  関数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目的:  メイン ウィンドウのメッセージを処理します。
+//
+//  WM_COMMAND	- アプリケーション メニューの処理
+//  WM_PAINT	- メイン ウィンドウの描画
+//  WM_DESTROY	- 中止メッセージを表示して戻る
+//
+//
+NOTIFYICONDATA task;
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_CREATE:
+		task.cbSize = sizeof(NOTIFYICONDATA);
+		task.hWnd = hWnd;
+		task.uID = 101;
+		task.hIcon = LoadIcon(hInst, (LPCTSTR)IDI_LISAGE);
+		task.uCallbackMessage = WM_TRAYICON;
+		lstrcpy(task.szTip, szTitle);
+		task.uFlags = NIF_ICON | NIF_MESSAGE | NIF_TIP;
+		Shell_NotifyIcon(NIM_ADD, &task);
+
+	case WM_COMMAND:
+		wmId    = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+		// 選択されたメニューの解析:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: 描画コードをここに追加してください...
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_TRAYICON:
+		if (lParam == WM_RBUTTONUP && wParam == 101)
+		{
+			// ポップアップメニューを出す
+			POINT cursor;
+			HMENU hMenu = LoadMenu(hInst, (LPCTSTR)IDC_LISAGE);
+
+			GetCursorPos(&cursor);
+			SetForegroundWindow(hWnd);
+			TrackPopupMenu(GetSubMenu(hMenu, 0),		// ポップうｐ
+							TPM_BOTTOMALIGN, cursor.x, cursor.y,
+							0, hWnd, NULL);
+			DestroyMenu(hMenu);
+		}
+		break;
+	case WM_DESTROY:
+		Shell_NotifyIcon(NIM_DELETE, &task);
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// バージョン情報ボックスのメッセージ ハンドラーです。
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
